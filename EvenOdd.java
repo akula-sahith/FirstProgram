d2151ce9b772f41625aaa9af78649aa28c49c010
Sahith Akula <akulasahith268@gmail.com>
2023-11-13 17:15:21 +0530

    a new program

---
+++ EvenOdd.java
@@ -0,0 +1,13 @@
+package FirstProgram;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 5;
+        if(n%2==0){
+            System.out.println("YEs");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
